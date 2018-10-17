09c7671ad8f7b7b6edaa9c81e0d14068f6a83a7b
LstChanceMadrid <michael.madrid@live.com>
2018-10-16 21:57:33 -0500

    adding styling

--- main.js
+++ main.js
@@ -1,71 +1,75 @@
 
 let createOrderEmailInput = document.getElementById('email-input');
 
-
 let coffeeSelection = document.getElementById('coffee-list').options;
-
 
 const COFFEE_ORDERS_URL = "http://dc-coffeerun.herokuapp.com/api/coffeeorders/";
 
-
-
 let orderEmail = document.getElementById('order-email');
 
-let orderInfoButton = document.getElementById('order-info-button')
+let orderInfoButton = document.getElementById('order-info-button');
+
 
 // displays all orders
-fetch(COFFEE_ORDERS_URL)
-.then((resp) => resp.json())
-.then(function(orders) {
-    let allOrders = document.getElementById('all-orders')
 
-    Object.keys(orders).map(e => {
+const updateOrders = () => {
+    fetch(COFFEE_ORDERS_URL)
+    .then((resp) => resp.json())
+    .then(function(orders) {
+        let pendingOrders = document.getElementById('pending-orders');
 
-        
+        pendingOrders.innerHTML = "";
+
+        Object.keys(orders).map(e => {
+                
         let eachOrder = `
-        <div>${orders[e].emailAddress}</div>
-        <div>${orders[e].coffee}</div>
-        `
-        allOrders.insertAdjacentHTML('beforeend',eachOrder)
-    })
-})
+            <div class="pending-order">
+                <div class="pending-orders-email"><span>Email:</span><br />${orders[e].emailAddress}</div>
+                <div class="pending-orders-coffee"><span>Coffee:</span><br />${orders[e].coffee}</div>
+            </div>
+            `;
 
+            pendingOrders.insertAdjacentHTML('beforeend', eachOrder);
+        });
+            
+    });
 
+}
 
 
 // Display the requested email's order
 
 orderInfoButton.addEventListener('click', function() {
-    let customerEmail = orderEmail.value
+    let customerEmail = orderEmail.value;
 
-    const COFFEE_ORDERS_BY_EMAIL_URL = `http://dc-coffeerun.herokuapp.com/api/coffeeorders/${customerEmail}`
+    const COFFEE_ORDERS_BY_EMAIL_URL = `http://dc-coffeerun.herokuapp.com/api/coffeeorders/${customerEmail}`;
 
     fetch(COFFEE_ORDERS_BY_EMAIL_URL)
     .then(function(order) {
-        console.log(order)
         return order.json();
     })
     .then(function(email) {
         let orderInformationContainer = document.getElementById('order-information-container');
 
         let orderInformation = 
-            `<div>${email.emailAddress}</div>
-            <div>${email.coffee}</div>
-            <label>Delete Order: <label>
-            <input id="delete-order"  type="submit" value="Delete" />
-            `
+            `<div class="order-email">${email.emailAddress}</div>
+            <div class="order-coffee">${email.coffee}</div>
+            <label class="order-label">Delete Order: <label>
+            <input id="delete-order" class="delete-order-button" type="submit" value="Delete" />
+            `;
             
-        orderInformationContainer.innerHTML = orderInformation  
+        orderInformationContainer.innerHTML = orderInformation;
+
 
         // deletes the called order
-        let deleteOrder = document.getElementById('delete-order')
+
+        let deleteOrder = document.getElementById('delete-order');
             
         deleteOrder.addEventListener('click', function() {
             fetch(COFFEE_ORDERS_BY_EMAIL_URL, {
                 method: 'DELETE'
             });
             orderInformationContainer.innerHTML = "";
-            console.log(COFFEE_ORDERS_URL)
         });
 
     })  
@@ -73,17 +77,16 @@ orderInfoButton.addEventListener('click', function() {
 });
 
 
-
-
-
 let submitButton = document.getElementById('submit-button');
 
+
 // Submits the new order to the json
+
 submitButton.addEventListener('click', function() {
     let correctCoffee = coffeeSelection[coffeeSelection.selectedIndex].text;
+
     let orderEmail = createOrderEmailInput.value;
-    console.log(correctCoffee)
-    console.log(orderEmail);
+
     fetch(COFFEE_ORDERS_URL, {  
         method: 'POST',  
         headers: {
@@ -93,5 +96,14 @@ submitButton.addEventListener('click', function() {
             emailAddress: orderEmail,
             coffee: correctCoffee
         })
-    })
+
+    });
+
 });
+
+updateOrders();
+
+setInterval(function() {
+
+    updateOrders();
+}, 5000);
